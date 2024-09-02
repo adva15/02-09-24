0939495c693277d0b98210eef60e83581ef45209
adva15 <aadva5319@gmail.com>
2024-09-02 19:59:44 +0300

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+# start
+
+x: int = 4
+
+y: int = 8
+
+z: int = 10
+
+print(x + y + z)
+
+print(x * y * z)
+
+# stop
---
+++ exercise2.py
@@ -0,0 +1,11 @@
+# start
+
+x: float = 2.4
+
+y: float = 4.5
+
+diff: float = float(input(x - y))
+
+print(diff)
+
+# stop
